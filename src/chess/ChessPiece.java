9ad1e256ab53996459df3653fc11e1ba7dba48f6
Amadeus Ferro <amadeus.ferro55@gmail,com>
2024-01-13 19:17:30 -0300

    Adding final features

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,19 +6,36 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-    private Color color;
+	private Color color;
+	private int moveCount;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    public Color getColor() {
-        return color;
-    }
-    protected boolean isThereOpponentPiece(Position position) {
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p != null && p.getColor() != color;
-    }
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
 
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
